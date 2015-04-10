0db578c7ea8d82f4c0e84b711c352ae2071d438f
Raimon Grau <raimon@3scale.net>
2015-04-10 20:45:50 +0100

    Basic parsing of the dash user-defined packages file

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
+                 [cheshire "5.4.0"]
+                 [http-kit "2.1.16"]
+                 [com.taoensso/carmine "2.9.2"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [environ "0.5.0"]]
   :min-lein-version "2.0.0"
--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -1,7 +1,10 @@
 (ns clojure-getting-started.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
+            [cheshire.core :refer :all]
             [compojure.route :as route]
+            [org.httpkit.client :as http]
+            [taoensso.carmine :as car :refer (wcar)]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]))
@@ -9,11 +12,34 @@
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body (pr-str ["Hello" :from 'Heroku])})
+   :body (generate-string ["foo" :from 'Heroku])})
+
+
+(defn contribs []
+  (let [{:keys [status headers body error] :as resp} @(http/get "http://sanfrancisco.kapeli.com/feeds/zzz/user_contributed/build/index.json")
+        packages (packages body)
+        ]
+   {:status 200
+    :headers {"Content-Type" "text/plain"}
+    :body packages
+    }))
+
+(defn packages [json-pkgs]
+  (let [docsets (:docsets (parse-string json-pkgs true))
+        url "http://newyork.kapeli.com/feeds/zzz/user_contributed/build/%s/%s"
+        docs (map (fn [x] (select-keys x [:name :archive]))
+                  (map #(get docsets %1)
+                       (keys docsets)))]
+    (generate-string
+     (map #(assoc-in %1 [:archive]
+                     (format url (:name %1) (:archive %1)))
+          docs))))
 
 (defroutes app
   (GET "/" []
        (splash))
+  (GET "/contrib-docs" []
+         (contribs))
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
