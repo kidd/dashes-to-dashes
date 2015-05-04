575c81760e3e783ca732a6d9a7c731639f91def1
Raimon Grau <raimon@3scale.net>
2015-05-05 00:42:01 +0100

    Add support for official docsets

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -8,6 +8,8 @@
             [clojure.java.io :as io]
             [environ.core  :refer [env]]
             [ring.adapter.jetty :as jetty]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip]
             [environ.core :refer [env]]))
 
 (def server1-conn {:pool {} :spec {:uri (env :redistogo-url)}}) ; See `wcar` docstring for opts
@@ -35,23 +37,57 @@
          (car/expire k exp))
   v)
 
-(defn get-json []
+(defn usercontrib-json []
   (let [redis-doc (or (wcar* (car/get json-redis-key))
                       (let [{:keys [status headers body error] :as resp}
                              @(http/get "http://sanfrancisco.kapeli.com/feeds/zzz/user_contributed/build/index.json")]
                         (set-expire json-redis-key (packages body) 3600)))]
      redis-doc))
 
-(defn docsets-contrib []
+(defn parse [s]
+   (xml/parse
+    (java.io.ByteArrayInputStream. (.getBytes s))))
+
+(defn prepare-json [xml]
+  (map (fn [ds]
+        (let [name (:name ds)
+              archive (-> (filter #( = (:tag %1) :url) (-> @(http/get (:download-url ds))
+                                                         :body
+                                                         parse
+                                                         :content))
+                        first :content first)]
+          {:name name
+           :archive archive}
+
+          ))
+       xml))
+
+(defn official-json []
+  (let [feed "https://api.github.com/repos/Kapeli/feeds/contents/"
+        docsets-url "https://raw.github.com/Kapeli/feeds/master"
+        docsets (parse-string (:body @(http/get feed)))
+        ds-symbolized-keys (map (fn [x] {:name (get x "name") :download-url (get x "download_url")}) docsets)
+        ds-only-xml (filter (fn [x] (re-find  #".xml$" (:name x)))  ds-symbolized-keys)
+        ignored-docsets (set ["Bootstrap.xml" "Drupal.xml" "Zend_Framework.xml" "Ruby_Installed_Gems.xml" "Man_Pages.xml"])
+        ds-filter-ignored (filter #(not (contains? ignored-docsets (:name %1))) ds-only-xml)
+        res (prepare-json ds-filter-ignored)]
+    res))
+
+(defn docsets [f]
   {:status 200
    :headers {"Content-Type" "application/json"}
-   :body (get-json)})
+   :body (f)})
+
+
+
 
 (defroutes app
   (GET "/" []
        (splash))
   (GET "/docsets/contrib" []
-       (docsets-contrib))
+       (docsets usercontrib-json))
+  (GET "/docsets/official" []
+       (docsets official-json))
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
